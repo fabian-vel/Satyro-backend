71dd4ee56682e36bc6225284b7143b0b92effc41
FabianVelasquez <favelasquez@softcaribbean.com>
2024-11-09 10:58:48 -0500

    Primer commit

---
+++ src/main/java/com/personal_project/satyro_backend/SatyroApplication.java
@@ -0,0 +1,13 @@
+package com.personal_project.satyro_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatyroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatyroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal_project/satyro_backend/SatyroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal_project.satyro_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatyroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
